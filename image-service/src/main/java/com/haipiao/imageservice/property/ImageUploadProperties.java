b7514013da5797d6fbde1ae23b54b638cdfc8fd3
Mengqi Yu <yuhaipiao@gmail.com>
2020-01-30 22:27:05 -0800

    update properties location

--- image-service/src/main/java/com/haipiao/imageservice/property/ImageUploadProperties.java
+++ image-service/src/main/java/com/haipiao/imageservice/property/ImageUploadProperties.java
@@ -1,7 +1,6 @@
 package com.haipiao.imageservice.property;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
@@ -10,7 +9,6 @@ import javax.validation.constraints.NotEmpty;
 @Component
 @Validated
 @ConfigurationProperties
-@PropertySource("classpath:/resources/imagesvc.properties")
 public class ImageUploadProperties {
 
     private STS sts;
