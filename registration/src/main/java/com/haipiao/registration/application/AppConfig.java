318e637245d0c6ef8f5b9fbd4a3164035856f328
Zhongyi Fan <zhongyifan@zhongyidembp.lan>
2020-02-05 23:23:13 -0800

    fix logging

--- registration/src/main/java/com/haipiao/registration/application/AppConfig.java
+++ registration/src/main/java/com/haipiao/registration/application/AppConfig.java
@@ -4,18 +4,30 @@ import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.haipiao.common.config.CommonConfig;
 import com.haipiao.common.enums.SecurityCodeType;
+import com.haipiao.common.interceptor.LoggingInterceptor;
 import com.haipiao.common.redis.RedisClientWrapper;
 import com.haipiao.common.util.security.SecurityCodeConfig;
 import com.haipiao.common.util.security.SecurityCodeManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Import(CommonConfig.class)
 @ComponentScan(basePackages = {"com.haipiao.registration.property", "com.haipiao.registration.handler"})
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
 
     @Bean
     public SecurityCodeManager securityCodeManager(Gson gson,
